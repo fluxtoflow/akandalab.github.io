0869df23df604411e8351a1cc065ecc08b5f46e9
albertlarson <albertl@uri.edu>
2021-11-02 10:09:11 -0400

    pubs 1

--- script.js
+++
@@ -1,8 +0,0 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js" type="text/javascript"></script>
-<script>
-$("#nav a").click(function(e){
-    e.preventDefault();
-    var toUpdate = $(this).attr('data-infoto');
-    $("#" + toUpdate).html($(this).text());
-});
-</script>
